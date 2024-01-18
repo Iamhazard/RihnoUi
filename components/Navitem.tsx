0e5d8a720a20c2724f211c9bd0b6aead54ff5ce2
Amrit Acharya <Amrithazard@gmail.com>
2024-01-18 20:23:40 +0545

    2nd

--- components/Navitem.tsx
+++ components/Navitem.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React from "react";
-import { Button } from "./ui/button";
-
 import { PRODUCT_CATEGORIES } from "@/config";
+import { Button } from "./ui/button";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 type Category = (typeof PRODUCT_CATEGORIES)[number];
 
@@ -17,10 +17,16 @@ interface NavItemProps {
   isAnyOpen: boolean;
 }
 
-const Navitem = ({ isAnyOpen, isOpen, handleOpen, category }: NavItemProps) => {
+const NavItem = ({
+  isAnyOpen,
+  category,
+  handleOpen,
+  close,
+  isOpen,
+}: NavItemProps) => {
   return (
     <div className="flex">
-      <div className="relative flex  items-center">
+      <div className="relative flex items-center">
         <Button
           className="gap-1.5"
           onClick={handleOpen}
@@ -33,8 +39,57 @@ const Navitem = ({ isAnyOpen, isOpen, handleOpen, category }: NavItemProps) => {
           />
         </Button>
       </div>
+
+      {isOpen ? (
+        <div
+          onClick={() => close()}
+          className={cn(
+            "absolute inset-x-0 top-full text-sm text-muted-foreground",
+            {
+              "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen,
+            }
+          )}>
+          <div
+            className="absolute inset-0 top-1/2 bg-white shadow"
+            aria-hidden="true"
+          />
+
+          <div className="relative bg-white">
+            <div className="mx-auto max-w-7xl px-8">
+              <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                  {category.featured.map((item) => (
+                    <div
+                      onClick={() => close}
+                      key={item.name}
+                      className="group relative text-base sm:text-sm">
+                      <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                        <Image
+                          src={item.imageSrc}
+                          alt="product category image"
+                          fill
+                          className="object-cover object-center"
+                        />
+                      </div>
+
+                      <Link
+                        href={item.href}
+                        className="mt-6 block font-medium text-gray-900">
+                        {item.name}
+                      </Link>
+                      <p className="mt-1" aria-hidden="true">
+                        Shop now
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
 
-export default Navitem;
+export default NavItem;
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { use } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "./button";
+import { Ghost } from "lucide-react";
+import Cart from "./Cart";
 
 const Navbar = () => {
+  const user = null;
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
@@ -18,8 +22,49 @@ const Navbar = () => {
                   <Icons.logo className="h-8 w-8" />
                 </Link>
               </div>
-              <div className="hidden z-50 lg:ml-8 lg:self-stretch">
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({ variant: "ghost" })}>
+                      Sign in{" "}
+                    </Link>
+                  )}
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}>
+                      Register
+                    </Link>
+                  )}
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
